a4eb05aab7f5bd6f694d8885e55819ebe0697af4
inioluwa14 <63956918+inioluwa14@users.noreply.github.com>
2023-04-17 23:14:46 +0100

    updated - fetched data from the api

---
+++ src/components/MovieCard.js
@@ -0,0 +1,67 @@
+// import logo from '../images/star-wars.png';
+import './MovieCard.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+
+const movieData = "https://swapi.dev/api/films";
+
+const MovieCard = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+     const getMovie = async () => {
+      try{
+       const response = await axios.get(movieData);
+      //  console.log(response.data.results);
+       setData(response.data.results);
+       setError(null);
+      }
+      catch(err){
+        setError(err.message);
+        setData(null);
+      }
+      finally{
+        setLoading(false);
+      }
+     }
+
+     getMovie();
+  },[])
+
+  return (
+    <div>
+      <h1 className='star-wars-logo'>STAR WARS</h1>
+      {loading && <div className='loading-icon'>
+         <i class="bi bi-arrow-clockwise"></i>
+      </div>}
+      {error && <div>{`There seems to be a problem fetching this data - ${error}`}</div>}
+      
+      <div className='movie'>
+        {data && data.map((item) => {
+          return (
+            
+          <div className='movie-container' key={item.episode_id}>
+            <div className='movie-title'>
+              <h3 className='title'>{item.title}</h3>
+              <span className='date-text'>{item.release_date}</span>
+            </div>
+            <p className='movie-description'>
+                {item.opening_crawl.substring(0,260)}...
+              </p>
+              <p className='more-button'>
+                <a href='##'>More Info</a>
+              </p>
+            </div>
+            )
+          })
+          
+        }
+      </div>
+    </div>
+  )
+}
+
+export default MovieCard
